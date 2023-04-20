dbae48dfa9b0ee8c0d8fbb241718ab361d6d546e
cantarelliana <accffers@gmail.com>
2023-04-19 21:10:44 -0300

    subindo exercício de variáveis

---
+++ main.js
@@ -0,0 +1,84 @@
+// const nome = prompt("Digite seu nome")
+// const idade = Number(prompt("Digite sua idade"))
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// 1 de interpretação
+/*
+let a = 10 
+let b = 10 
+console.log(b)
+b = 5
+console.log(a, b)
+
+2 de interpretação, c = 10, b = 10, c = 10
+let a = 10
+let b = 20
+c = a 
+b = c
+a = b
+console.log(a, b, c)
+
+3 de interpretação
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+*/
+
+// 1 de escrita
+/*
+let nome 
+let idade
+
+console.log(typeof nome, typeof idade)
+
+let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+
+console.log(typeof nome, typeof idade)
+
+// os dois typeof deram string
+
+console.log(`Olá ${nome}, você tem ${idade} anos`)
+*/
+
+// 2 de escrita
+/*
+let azul = prompt("Você está usando roupa azul hoje?")
+
+let preta = prompt("Você está usando roupa preta hoje?")
+let branca = prompt("Você está usando roupa branca hoje?")
+
+
+console.log(`Você está usando roupa azul hoje? - ${azul}`)
+console.log(`Você está usando roupa preta hoje? - ${preta}`)
+console.log(`Você está usando roupa branca hoje? - ${branca}`)
+*/
+
+// 3 de escrita 
+/*
+let a = 10
+let b = 25
+
+// c = 25, b = 10
+
+c=b
+b=a
+a=c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+// desafio
+/*
+pri_n = Number(prompt("digite o primeiro número"))
+seg_n = Number(prompt("digite o segundo número"))
+
+soma = pri_n + seg_n
+mult = pri_n * seg_n
+
+console.log(`O primeiro número somado ao segundo número resulta em: ${soma}`)
+console.log(`O primeiro número multiplicado pelo segundo número resulta em: ${mult}`)
+*/
